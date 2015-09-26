2a29d836830ad206ed3f709a841b0d038e206904
ulve <johansson.olov@gmail.com>
2015-09-26 07:44:27 +0200

    Flytta
    Flytta

---
+++ Program.fs
@@ -0,0 +1,43 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Suave
+open Suave.Http
+open Suave.Http.Applicatives
+open Suave.Http.Successful
+open Suave.Web
+open System.IO
+
+let app = 
+    choose 
+        [ GET >>= choose
+            [ path "/hello" >>= OK "Hello GET" 
+              path "/goodby" >>= OK "Godby GET" ]
+          POST >>= choose
+            [ path "/hello" >>= OK "Hello POST" 
+              path "/goodby" >>= OK "Goodby POST" ] ]
+
+let rec getAllFiles dir pattern =
+    seq { yield! Directory.EnumerateFiles(dir, pattern)
+          for d in Directory.EnumerateDirectories(dir) do
+              yield! getAllFiles d pattern }
+
+type sökväg = { sökväg : string; filnamn: string }
+
+let convertToSökväg grund (helSökväg : string) =
+    let utanGrund = helSökväg.Replace(grund, "")
+    let s = { sökväg = utanGrund; filnamn = grund }
+    s 
+
+[<EntryPoint>]
+let main argv = 
+    getAllFiles "c:\\temp\\doc" "*.txt" 
+    //|> Seq.map (fun f -> f.Replace("c:\\temp\\doc+\\", "")) 
+    |> Seq.map (convertToSökväg "c:\\temp\\doc\\")
+    |> Seq.cast 
+    |> printfn "%A"
+    printfn "%A" argv
+    startWebServer defaultConfig app
+
+    0 // return an integer exit code
+
